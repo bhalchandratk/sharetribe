1ec3d4b64dc7644ecb01dec636748d1cd948308f
Nikolai <devnik@ithouse.lv>
2017-08-04 12:54:36 +0300

    updated jquery.ui.touch-punch.js for better handling of clicks over draggable images in mobile

--- db/structure.sql
+++ db/structure.sql
@@ -2131,6 +2131,3 @@ INSERT INTO `schema_migrations` (version) VALUES
 ('20170613153961'),
 ('20170613153965'),
 ('20170626065542');
-
-
-INSERT INTO schema_migrations (version) VALUES ('20170613153965');
